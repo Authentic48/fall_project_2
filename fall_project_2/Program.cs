311d8b96134b0399a8ea31e624c3c2dc52e9d60e
Authentic48 <iboufall48@gmail.com>
2022-11-07 09:47:23 +0700

    Basic CLI Menu added

--- fall_project_2/Program.cs
+++ fall_project_2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Welcome to your personal finance console app");
+Console.WriteLine("Choose an option");
+Console.WriteLine("[1] register");
+Console.WriteLine("[2] login");
+Console.WriteLine("[3] exit");
+Console.Write("\r\nSelect an option: ");
+
+switch (Console.ReadLine())
+{
+    case "1":
+        Console.WriteLine("This is the register option");
+        break;
+    case "2":
+        Console.WriteLine("This is the login option");
+        break;
+    case "3":
+        Console.WriteLine("GoodBye!");
+        break;
+}
