c3b1d20eaac88edcec7e7c54e81d194c8ff9e6a0
redouane <redouane.sobaihi@live.com>
2022-11-09 00:28:50 +0300

    add CollectStatistic implementation and guard for AddOperation

--- fall_project_2/Wallet.cs
+++ fall_project_2/Wallet.cs
@@ -20,16 +20,48 @@ public class Wallet
 
     public Money Amount { get; }
 
-    public void AddOperation(Income operation)
+    public void AddOperation(Operation operation)
     {
+        if (operation.Value.Currency != this.Currency)
+        {
+            // TODO: throw custom exception indicating the wallet does not support the operation currency
+            throw new Exception();
+        }
+
         Operations.Add(operation);
     }
 
-    public void AddOperation(Expense operation)
+    public void CollectStatistic(DateTime from, DateTime to)
     {
-        Operations.Add(operation);
+        var totalIncomeOperations = this.Operations.OfType<Income>().Count();
+        var incomeOperations = this.Operations
+            .OfType<Income>()
+            .Where(operation => operation.Date >= from && operation.Date <= to)
+            .GroupBy(operation => operation.Type)
+            .Select(operation => new
+            {
+                Type = operation.Key,
+                Overview = operation.Count() / totalIncomeOperations,
+                Total = operation.Count(),
+                Operations = operation.ToArray()
+            })
+            .ToArray();
+
+        var totalExpenseOperations = this.Operations.OfType<Expense>().Count();
+        var expenseOperations = this.Operations
+            .OfType<Expense>()
+            .Where(operation => operation.Date >= from && operation.Date <= to)
+            .GroupBy(operation => operation.Type)
+            .Select(operation => new
+            {
+                Type = operation.Key,
+                Overview = operation.Count() / totalIncomeOperations,
+                Total = operation.Count(),
+                Operations = operation.ToArray()
+            })
+            .ToArray();
+
+        // TODO: build string to print operations aggregates
     }
 
-    public void CollectStatistic(DateTime from, DateTime to) { }
-    
 }
